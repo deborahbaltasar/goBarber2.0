2e89bde833b9ae886a7ff8cc9690acf4a54d6c65
Deborah <deborahbnogueira.dn@gmail.com>
2020-11-08 15:43:08 -0300

    add toasts

---
+++ src/hooks/toast.js
@@ -0,0 +1,44 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import { v4 } from 'uuid';
+import ToastContainer from '../components/ToastContainer';
+
+const ToastContext = createContext({});
+
+const ToastProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+
+  const addToast = useCallback(({ type, title, description }) => {
+    const id = v4();
+
+    const toast = {
+      id,
+      type,
+      title,
+      description,
+    }
+
+    setMessages(oldMessages => [...oldMessages, toast])
+  }, []);
+
+  const removeToast = useCallback((id) => {
+    setMessages(state => state.filter(message => message.id !== id));
+  }, []);
+
+  return (
+    <ToastContext.Provider value={{addToast, removeToast}}>
+      {children}
+      <ToastContainer  messages={messages} />
+    </ToastContext.Provider>
+  )}
+
+const useToast = () => {
+  const context = useContext(ToastContext);
+
+  if(!context) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+
+  return context;
+}
+
+export { ToastProvider, useToast };
